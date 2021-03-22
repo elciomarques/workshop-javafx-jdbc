bd853fe054613d33b3f0a5f72198fe5125bbe43d
elciomarques <elcio@elcio.info>
2021-03-22 12:29:51 -0300

    Main view desing

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,8 +13,15 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load();
-			Scene mainScene = new Scene(parent);
+			
+			//colocou a função scrollpane do javafx
+			ScrollPane scrollpane = loader.load();
+			
+			// redimensionar o menu toobar
+			scrollpane.setFitToHeight(true);
+			scrollpane.setFitToWidth(true);
+			 
+			Scene mainScene = new Scene(scrollpane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
